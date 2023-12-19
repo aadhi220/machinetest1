bd18e687d01e5ec26c6aaa56578a04f23d1b3ad7
Adithyan Anil <adithyananil2002@gmail.com>
2023-12-20 01:07:53 +0530

    header primary design

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+import logo from '../../../public/logo.png'
+function Header() {
+  return (
+    <div className="header-container ">
+<div className='brand-container'>
+ <div className='brand-logo-box'> <img src={logo} className='brand-logo' alt="" /></div>
+ 
+</div>
+<div className=''>
+
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default Header
